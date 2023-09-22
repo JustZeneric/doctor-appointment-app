9052414005784edca53836842b6e1923c173170a
JustZeneric <lukemanley48@gmail.com>
2023-09-22 16:29:48 +0200

    update

--- server/server.js
+++ server/server.js
@@ -13,12 +13,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware code
 app.use(cors());
 app.use(express.json());
-
+mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-mongoose.set('strictQuery', false);
+
 
 mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB');
