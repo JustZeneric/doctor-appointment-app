b08eb5624be27f3f0cbc0f9eff26180c0213409c
JustZeneric <lukemanley48@gmail.com>
2023-09-22 15:23:38 +0200

    Update

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const adminRoutes = require('./routes/adminRoutes');
@@ -13,12 +14,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB using environment variables
-mongoose.connect('mongodb+srv://admin:1234@tasknest.orfrlpg.mongodb.net/MangApp', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
 
 mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB');
