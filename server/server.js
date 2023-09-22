c04f981336db261b5c51381fd2d0aac02b24a63b
JustZeneric <lukemanley48@gmail.com>
2023-09-22 17:44:35 +0200

    update

--- server/server.js
+++ server/server.js
@@ -42,7 +42,3 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
