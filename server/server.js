2edbadad72b563905b985ae96c662b85561fbeed
JustZeneric <lukemanley48@gmail.com>
2023-09-22 16:49:22 +0200

    update

--- server/server.js
+++ server/server.js
@@ -13,8 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware code
 app.use(cors());
 app.use(express.json());
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect('mongodb+srv://admin:1234@tasknest.orfrlpg.mongodb.net/MangApp', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
